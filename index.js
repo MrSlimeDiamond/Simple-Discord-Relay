5b276f4720138d0dda1c1ab16ef4833954dbfd3b
MrSlimeDiamond <findlayrichardson3@gmail.com>
2021-03-11 18:11:12 +1300

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const config = require("./config.json");
+const Discord = require("Discord.js");
+const irc = require("irc");
+
+var ircclient = new irc.Client(config.ircserver, config.ircnick, {
+    channels: [config.channels],
+});
+var discordclient = new Discord.Client();
+
+discordclient.login(config.discordtoken);
+
+ircclient.addListener("message", (from, to, text, message) => {
+    if (from == config.ircnick) return;
+    discordclient.channels.cache.get(config.discordchannel).send(`[IRC] <${from}> ${text}`);
+});
+
+discordclient.on("message", function (message) {
+    if (message.webhookID) return;
+    if (message.author.id == config.discordbotid) return;
+    if (message.channel.id == config.discordchannel) {
+        ircclient.say(config.channels, `[DISCORD] <${message.author.tag}> ${message}`);
+        return;
+    }
+});
